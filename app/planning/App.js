e66f174314bb4ae9bca2fbfffab0d262d8fabef7
fermanlive <fernando.link32@gmail.com>
2020-01-26 22:17:07 -0500

    ajustes de tab

--- app/planning/App.js
+++ app/planning/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createAppContainer,createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import HomeScreen from './src/screens/home';
 import LoginScreen from './src/screens/login';
 import RegisterScreen from './src/screens/register';
@@ -9,8 +10,8 @@ import ForgetPasswordScreen from './src/screens/forgetPassword';
 import ProfileScreen from './src/screens/profile';
 
 
-
-const homeStack = createStackNavigator({
+const homeStack = createBottomTabNavigator({
+//const homeStack = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
@@ -24,7 +25,16 @@ const homeStack = createStackNavigator({
     }
   },
 },{
-  initialRouteName:'Home'
+  initialRouteName:'Home',
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+    labelStyle: {
+      fontSize: 12,
+    },
+    style: {
+      backgroundColor: 'blue',
+    },
+  }
 }
 );
 
