52c55dd1349f474e6f278ae0e1da67b3b774730a
Daniel Murcia <daniel.murcia@code-labs.com>
2020-02-07 11:51:57 -0500

    Ajustes de Diccionario

--- app/planning/src/screens/dictionary.js
+++ app/planning/src/screens/dictionary.js
@@ -6,13 +6,7 @@ class Dictionary extends React.Component {
     render() {
       return(
           <View style={ [layout.MainContainer, layout.AlignCenter] }>
-            <TouchableOpacity 
-                onPress={() => this.props.navigation.navigate('home')}
-                style={[buttons.GralButton, buttons.BLightBlue]}>
-                <Text style={[text.BText, text.TLight]}>
-                    Regresar al home
-                </Text>
-            </TouchableOpacity>
+
           </View>
     );
   }
