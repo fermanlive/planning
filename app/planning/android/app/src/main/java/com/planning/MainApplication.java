b5e587801ee9c42047aa624ec4d4eafda0dea895
fermanlive <fernando.link32@gmail.com>
2020-05-14 09:43:25 -0500

    ajustes de integracion de servicios y terminacion de simulator

--- app/planning/android/app/src/main/java/com/planning/MainApplication.java
+++ app/planning/android/app/src/main/java/com/planning/MainApplication.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 
-import co.apptailor.googlesignin.RNGoogleSigninPackage;  // <--- import
-
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
